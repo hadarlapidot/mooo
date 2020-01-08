0d3d3a0899b6fd5ecefcaa614e823b5dd51570d9
hadar ram lapidot <hadarlapidot@gmail.com>
2020-01-08 15:49:56 +0200

    Updates

---
+++ precache-manifest.758c0f77857e2bd6f3058dc7c6f55b3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2391059a397faff1e4794145c09ce5",
+    "url": "/mooo/index.html"
+  },
+  {
+    "revision": "c6bfba6e9b268a83f6a7",
+    "url": "/mooo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a709f68328111ac9f5a4",
+    "url": "/mooo/static/css/main.1e3c9fcd.chunk.css"
+  },
+  {
+    "revision": "c6bfba6e9b268a83f6a7",
+    "url": "/mooo/static/js/2.9b0e3121.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mooo/static/js/2.9b0e3121.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a709f68328111ac9f5a4",
+    "url": "/mooo/static/js/main.f1a10286.chunk.js"
+  },
+  {
+    "revision": "ffa13f03a7e8345228ff",
+    "url": "/mooo/static/js/runtime-main.8213f75c.js"
+  }
+]);
